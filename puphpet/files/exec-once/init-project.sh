9f9e62e66276a96f38badf897914c097d09d5357
Géza Búza <bghome@gmail.com>
2015-03-11 00:39:48 +0100

    Composer failed to run during provisioning
    There is no "bin" directory in the project root, so we cannot run composer from there. However the VM has it, so run it from there.

--- puphpet/files/exec-once/init-project.sh
+++ puphpet/files/exec-once/init-project.sh
@@ -3,8 +3,7 @@ echo "cd /var/www/szepul.hu" >> /home/vagrant/.bash_profile
 cd /var/www/szepul.hu
 rm -rf app/cache/*
 rm -rf app/logs/*
-bin/composer.phar self-update
-bin/composer.phar install
+composer install
 app/console doctrine:schema:create
 app/console doctrine:fixtures:load --no-interaction
 app/console assets:install --symlink
