a06b66f8f70cca183ad538efb6ff75355ab0d7a7
David Hargitai <dhargitai@sessiondigital.com>
2015-03-15 20:35:36 +0100

    Add command to install frontend dependencies and download media files for data fixtures

--- puphpet/files/exec-once/init-project.sh
+++ puphpet/files/exec-once/init-project.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 echo "cd /var/www/szepul.hu" >> /home/vagrant/.bash_profile
+wget -nv https://www.dropbox.com/s/ty3soyfivprjcp4/szepul.hu.fixtures.files.tar.gz?dl=0 -O /tmp/fixtures.tar.gz
 cd /var/www/szepul.hu
+sudo npm install -g bower &>npm-install.log
+
+cd app/Resources/public
+bower --config.interactive=false --allow-root install
+cd /var/www/szepul.hu
+
 rm -rf app/cache/*
 rm -rf app/logs/*
 mkdir -p app/data
 /usr/local/bin/composer --prefer-source --no-interaction install
 app/console doctrine:schema:create
+
+cd src/Application/DataFixtures/
+tar -xzf /tmp/fixtures.tar.gz --owner vagrant --group www-data --no-same-owner
+cd /var/www/szepul.hu
 app/console doctrine:fixtures:load --no-interaction
+
 app/console assets:install --symlink
 app/console assetic:dump
