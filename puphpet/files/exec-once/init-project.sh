161e2f29a17b2965bf01ffb35acf8138dbb14278
Géza Búza <bghome@gmail.com>
2015-03-15 19:37:59 +0100

    Set SQLite as default database backend

--- puphpet/files/exec-once/init-project.sh
+++ puphpet/files/exec-once/init-project.sh
@@ -3,6 +3,7 @@ echo "cd /var/www/szepul.hu" >> /home/vagrant/.bash_profile
 cd /var/www/szepul.hu
 rm -rf app/cache/*
 rm -rf app/logs/*
+mkdir -p app/data
 /usr/local/bin/composer install
 app/console doctrine:schema:create
 app/console doctrine:fixtures:load --no-interaction
