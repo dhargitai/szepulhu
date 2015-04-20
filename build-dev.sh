f5a482d4b156b9957a7b1d7dcbdfa8f8a71b0692
David Hargitai <dhargitai@sessiondigital.com>
2015-04-20 10:34:08 +0200

    Fix typo

--- build-dev.sh
+++ build-dev.sh
@@ -6,7 +6,7 @@ docker-compose run --rm front /bin/bash -c 'bin/wait-for-db.sh && \
                                             php app/console doctrine:schema:create && \
                                             wget https://www.dropbox.com/s/ty3soyfivprjcp4/szepul.hu.fixtures.files.tar.gz?dl=0 -O /tmp/fixtures.tar.gz && \
                                             tar -xzf /tmp/fixtures.tar.gz --no-same-owner -C src/Application/DataFixtures/ && \
-                                            mkdir -p web/uploads/media
+                                            mkdir -p web/uploads/media && \
                                             php app/console doctrine:fixtures:load --no-interaction && \
                                             cd app/Resources/public && bower --config.interactive=false --allow-root install && cd ../../.. && \
                                             php app/console assetic:dump' && \
