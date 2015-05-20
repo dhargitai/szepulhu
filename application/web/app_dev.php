fe8339ae5429d9a36a097f9345f49e63a504ea23
David Hargitai <dhargitai@sessiondigital.com>
2015-05-20 23:22:17 +0200

    Overwrite Symfony's error reporting to hide deprecated warnings

--- application/web/app_dev.php
+++ application/web/app_dev.php
@@ -18,7 +18,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-Debug::enable();
+Debug::enable(E_RECOVERABLE_ERROR & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED, false);
 
 require_once __DIR__.'/../app/AppKernel.php';
 
