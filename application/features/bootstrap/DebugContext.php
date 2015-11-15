ad13b134593877f801439262b86905e4086e3ecf
Géza Búza <bghome@gmail.com>
2015-11-15 18:32:55 +0100

    SZEPULHU-9: Install Behat Screenshot extension
    Replace our customer screenshot making implementation with the Behat Screesnhot extension.

--- application/features/bootstrap/DebugContext.php
+++ application/features/bootstrap/DebugContext.php
@@ -19,9 +19,6 @@ use Behat\Testwork\Tester\Result\TestResult;
  * This class is responsible for providing debug information to the developer about the running Behat tests.
  * It also can help configuring the web browser used for testing.
  *
- * When you need to debug a scenario step by step, then add the "@debug_step" tag to the feature file. This will
- * take a screenshot after every step.
- *
  * @author Geza Buza <bghome@gmail.com>
  */
 class DebugContext implements Context
@@ -35,9 +32,6 @@ class DebugContext implements Context
     /** @var \Behat\MinkExtension\Context\MinkContext */
     private $minkContext;
 
-    /** @var string $screenshotDirectoryName */
-    protected $screenshotDirectoryName;
-
     /** @var int $screenWidth Desired browser window width */
     protected $screenWidth;
 
@@ -47,12 +41,10 @@ class DebugContext implements Context
     /**
      * Constructor
      *
-     * @param string $screenshotDirectoryName Directory name of browser screenshots
      * @param string $windowResolution Desired browser window resolution in "1366x768" format
      */
-    public function __construct($screenshotDirectoryName, $windowResolution)
+    public function __construct($windowResolution)
     {
-        $this->screenshotDirectoryName = $screenshotDirectoryName;
         $resolution = explode('x', $windowResolution);
         $this->screenWidth = $resolution[0];
         $this->screenHeight = $resolution[1];
@@ -67,59 +59,5 @@ class DebugContext implements Context
 
         $this->minkContext = $environment->getContext('Behat\MinkExtension\Context\RawMinkContext');
         $this->minkContext->getSession()->resizeWindow($this->screenWidth, $this->screenHeight);
-    }
-
-    /**
-     * @AfterScenario
-     */
-    public function takeScreenshotAfterFailedScenario(AfterScenarioScope $scope)
-    {
-        if (TestResult::FAILED === $scope->getTestResult()->getResultCode()) {
-            $this->takeScreenshot($scope->getScenario()->getTitle(), $scope->getScenario()->getLine());
-        }
-    }
-    /**
-     * @AfterStep
-     */
-    public function takeScreenshotAfterStep(\Behat\Behat\Hook\Scope\AfterStepScope $scope)
-    {
-        if (in_array(self::TAG_TAKE_SCREENSHOT_AFTER_EVERY_STEP, $scope->getFeature()->getTags())) {
-            $this->takeScreenshot($scope->getStep()->getText(), $scope->getStep()->getLine());
-        }
-    }
-
-    /**
-     * @param string $scenarioName
-     * @param integer|null $line
-     */
-    private function takeScreenshot($scenarioName, $line = null)
-    {
-        $driver = $this->minkContext->getSession()->getDriver();
-        if (!$driver instanceof Selenium2Driver) {
-            return;
-        }
-        $fileName = $scenarioName . ($line ? "-$line" : '') . '.png';
-        $fileName = strlen($fileName) > self::MAX_FILENAME_LENGTH ? "scenario-$line.png" : $fileName;
-        $filePath = $this->getKernel()->getLogDir() . DIRECTORY_SEPARATOR . $this->screenshotDirectoryName;
-
-        $this->ensureDirectoryExists($filePath);
-        $this->minkContext->saveScreenshot($fileName, $filePath);
-
-        echo sprintf(
-            "Screenshot has been taken at %s\n" .
-            "and saved to %s",
-            $this->minkContext->getMinkParameter('base_url'),
-            $filePath . DIRECTORY_SEPARATOR . $fileName
-        );
-    }
-
-    /**
-     * @param string $filePath
-     */
-    private function ensureDirectoryExists($filePath)
-    {
-        if (!file_exists($filePath)) {
-            mkdir($filePath, 0775, true);
-        }
     }
 }
